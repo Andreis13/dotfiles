cf8af5804bcfeb807567563193ed0f738f916b9c
Andrei Istratii <andrei.istratii@gmail.com>
2021-07-20 13:24:26 +0300

    Add host to theme

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cp ~/dotfiles/.dircolors ~/.dircolors
+cp ~/dotfiles/.zshrc ~/.zshrc
+cp ~/dotfiles/zsh_themes/robbyrussell_with_host.zsh-theme ~/.oh-my-zsh/custom/themes/robbyrussell_with_host.zsh-theme
