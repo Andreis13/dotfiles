9769d3a3f5da1302b0748c5dc7ae2828463ea1db
Andrei Istratii <andrei.istratii@gmail.com>
2021-12-13 16:53:12 -0500

    Add iterm2 and karabiner configs

--- install.sh
+++ install.sh
@@ -5,3 +5,19 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 cp ~/dotfiles/.dircolors ~/.dircolors
 cp ~/dotfiles/.zshrc ~/.zshrc
 cp ~/dotfiles/zsh_themes/robbyrussell_with_host.zsh-theme ~/.oh-my-zsh/custom/themes/robbyrussell_with_host.zsh-theme
+
+
+# if executing on a MacOS machine
+if [[ $OSTYPE == 'darwin'* ]]; then
+  # Specify iTerm2 the preferences directory
+  defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm2"
+  defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
+  # Change screenshot directory
+  mkdir -p ~/Desktop/Screenshots
+  defaults write com.apple.screencapture location ~/Desktop/Screenshots
+
+  # Move Karabiner-Elements config
+  mkdir -p ~/.config/karabiner
+  cp ~/dotfiles/karabiner/karabiner.json ~/.config/karabiner/
+fi
